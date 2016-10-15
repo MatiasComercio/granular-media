fa1567f7d97f4a0c1b43d7ef8329cb175bb68919
mati <matimercado06@hotmail.com>
2016-10-15 11:44:45 -0300

    Improved Cim

--- services/src/main/java/ar.edu.itba.ss/granularmedia/services/neighboursfinders/BruteForceMethodImpl.java
+++ services/src/main/java/ar.edu.itba.ss/granularmedia/services/neighboursfinders/BruteForceMethodImpl.java
@@ -10,7 +10,7 @@ public class BruteForceMethodImpl implements NeighboursFinder {
 
   @Override
   public Map<Particle, Collection<Particle>> run(Collection<Particle> particles,
-                                                 double L, int M, double rc, boolean periodicLimit) {
+                                                 double L, double W, int M1, int M2, double rc, boolean periodicLimit) {
 
     final List<Particle> pointsAsList = new ArrayList<>(particles);
     final Map<Particle, Collection<Particle>> collisionPerParticle = new HashMap<>(particles.size());
@@ -32,10 +32,10 @@ public class BruteForceMethodImpl implements NeighboursFinder {
     double distance;
 
     for (int i = 0; i < pointsAsList.size(); i++) {
-      for (int j = i+1; j < pointsAsList.size(); j++) {
+      for (int j = 0; j < pointsAsList.size(); j++) {
         distance = Space2DMaths.distanceBetween(pointsAsList.get(i),
                 pointsAsList.get(j));
-        if (distance <= rc) {
+        if (distance <= rc && !pointsAsList.get(i).equals(pointsAsList.get(j))) {
           collisionPerParticle.get(pointsAsList.get(i)).add(pointsAsList.get(j));
           collisionPerParticle.get(pointsAsList.get(j)).add(pointsAsList.get(i));
         }
